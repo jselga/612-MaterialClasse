7c72f0915c3c89adabf4638736a0aa2d849bc733
jselga <jordi.selga@iescarlesvallbona.cat>
2025-11-22 21:02:29 +0100

    Bloc3: Primera app amb React  - Component Card amb Fragment

--- T2/Bloc3/my-first-app/src/components/Card.jsx
+++ T2/Bloc3/my-first-app/src/components/Card.jsx
@@ -4,17 +4,23 @@ function Card() {
     // El primer parentesi evalua una variable i el segon és per definir un objecte
     <div className="card" style={{ width: "18rem", height: "12rem" }}>
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card’s content.
-        </p>
-        <a href="#" className="btn btn-primary">
-          Go somewhere
-        </a>
+        <CardBody />
       </div>
     </div>
-
+  );
+}
+export function CardBody() {
+  return (
+    <>
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">
+        Some quick example text to build on the card title and make up the bulk
+        of the card’s content.
+      </p>
+      <a href="#" className="btn btn-primary">
+        Go somewhere
+      </a>
+    </>
   );
 }
 export default Card;
