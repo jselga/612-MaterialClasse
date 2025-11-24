d45b0ee110c4d3f64fab688decc95956d9a9ff6e
jselga <jordi.selga@iescarlesvallbona.cat>
2025-11-25 00:21:42 +0100

    Bloc3: Primera app amb React  - Funcions com a props

--- T2/Bloc3/my-first-app/src/App.jsx
+++ T2/Bloc3/my-first-app/src/App.jsx
@@ -2,10 +2,14 @@ import Card, { CardBody } from "./components/Card";
 import List from "./components/List";
 function App() {
   const list = ["Chopper", "Rocket", "Cosmo", "Ponyo"];
+  const handleSelect = (element)=> {
+    console.log('imprimint', element);
+    
+  }
   return (
     <Card>
       <CardBody title="Hola Card" text="Aquest és el text" />
-      <List data={list} />
+      <List data={list} onSelect={handleSelect}/>
       {/* <List data={list} /> */}
     </Card>
   );
--- T2/Bloc3/my-first-app/src/components/List.jsx
+++ T2/Bloc3/my-first-app/src/components/List.jsx
@@ -1,29 +1,26 @@
 import { useState } from "react";
-
+// @ts-check
 /**
  *
- * @param {{data: string []}} props
+ * @param {{data: string[],onSelect: (element:string)=>void}} props
  * @returns
  */
-function List({ data }) {
+function List({ data, onSelect }) {
   // state Hook [variable,setVariable]=useState(valorInicial)
- const  [index,setIndex]=useState(1);
-  // let index =1;
-  // const handleClick = (i) =>{
-  //    index=i;
-  //   }
-  const handleClick =(i)=>{
+  const [index, setIndex] = useState(1);
+  const handleClick = (i, el) => {
     setIndex(i);
-  }
+    onSelect(el);
+ 
+  };
   return (
     <div>
       <ul className="list-group">
-        {data.map((el,i) => (
+        {data.map((el, i) => (
           <li
-                    
-            onClick={()=>handleClick(i)}
+            onClick={() => handleClick(i, el)}
             key={el}
-            className={`list-group-item ${index==i ? 'active':'' }`}
+            className={`list-group-item ${index == i ? "active" : ""}`}
           >
             {el}
           </li>
