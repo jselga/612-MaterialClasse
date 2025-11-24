3dacfa15ed1766899bbf64101eab2373c5e1a7a2
jselga <jordi.selga@iescarlesvallbona.cat>
2025-11-25 00:35:25 +0100

    Bloc3: Primera app amb React  - Renderitzat condicional

--- T2/Bloc3/my-first-app/src/App.jsx
+++ T2/Bloc3/my-first-app/src/App.jsx
@@ -1,16 +1,38 @@
 import Card, { CardBody } from "./components/Card";
 import List from "./components/List";
+/** En  JS
+ * truthy:
+ *
+ * falsy:
+ *  0
+ *  ''
+ *  false
+ *  undefinded
+ *  nul
+ */
+/**
+ * En React no és així.
+ *  Compte amb el 0, no és un falsy
+ */
 function App() {
   const list = ["Chopper", "Rocket", "Cosmo", "Ponyo"];
-  const handleSelect = (element)=> {
-    console.log('imprimint', element);
-    
-  }
+  // const list=[];
+  const handleSelect = (element) => {
+    console.log("imprimint", element);
+  };
+  // const content =
+  //   list.length !== 0 ? (
+  //     <List data={list} onSelect={handleSelect} />
+  //   ) : (
+  //     "Sense elements per mostrar"
+  //   );
+  const content = list.length !== 0 && (
+    <List data={list} onSelect={handleSelect} />
+  );
   return (
     <Card>
       <CardBody title="Hola Card" text="Aquest és el text" />
-      <List data={list} onSelect={handleSelect}/>
-      {/* <List data={list} /> */}
+      {content}
     </Card>
   );
 }
