4930570eec57fbc65eb830bb1f6675150456e93b
jselga <jordi.selga@iescarlesvallbona.cat>
2025-11-30 19:37:42 +0100

    Formulari amb paràmetres

--- T2/Bloc3/forms-explained/src/components/Form.jsx
+++ T2/Bloc3/forms-explained/src/components/Form.jsx
@@ -1,13 +1,13 @@
+import { useState } from "react";
 function Form() {
+  const [user, setUser] = useState({ name: "", lastname: "" });
   /**
    *
    * @param {import("react").FormEvent} e
    */
-
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    console.log("Enviant...");
+    console.log(user);
   };
   return (
     // form>div.mb-3*2>label.form-label+input#name.form-control
@@ -16,13 +16,25 @@ function Form() {
         <label htmlFor="" className="form-label">
           Nom
         </label>
-        <input type="text" id="name" className="form-control" />
+        <input
+          value={user.name}
+          onChange={(e) => setUser({ ...user, name: e.target.value })}
+          type="text"
+          id="name"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="lastname" className="form-label">
           Cognom
         </label>
-        <input type="text" id="lastname" className="form-control" />
+        <input
+          value={user.lastname}
+          onChange={(e) => setUser({ ...user, lastname: e.target.value })}
+          type="text"
+          id="lastname"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary">Enviar</button>
     </form>
