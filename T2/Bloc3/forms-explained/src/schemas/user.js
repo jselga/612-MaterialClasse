4b1bf78548e535685ffa5f6fe5a63b5c5437cb1b
jselga <jordi.selga@iescarlesvallbona.cat>
2025-11-30 21:46:44 +0100

    Validacions amb ZOD i Resolvers

--- T2/Bloc3/forms-explained/src/schemas/user.js
+++ T2/Bloc3/forms-explained/src/schemas/user.js
@@ -1,7 +1,8 @@
 import { z } from 'zod';
 export const userSchema = z.object({
     name: z
-        .string().nonempty({ message: "El nom és obligatori" })
+        .string()
+        .min(1, { message: "El nom és obligatori" })
         .min(3, { message: "Longitud mínima de 3 caràcters" }),
     lastname:
         z
