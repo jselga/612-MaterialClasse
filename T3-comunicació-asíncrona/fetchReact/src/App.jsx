3ab449e1097b59089d1b9abd6fc14094848e710f
jselga <jordi.selga@iescarlesvallbona.cat>
2026-01-19 12:08:01 +0100

    React: Fetch basic d'usuaris amb

---
+++ T3-comunicació-asíncrona/fetchReact/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [data,setData] = useState(null);
+  const baseUrl='https://jsonplaceholder.typicode.com'
+  useEffect(()=>{
+    fetch(`${baseUrl}/users`)
+    .then((res)=> res.json())
+    .then((data)=> setData(data));
+  }, [])
+  return (
+    <div className="App">
+      <h1>Fetch Like a PRO</h1>
+      <div className="card">
+        <ul>
+          {data?.map ((user)=> <li key={user.id}>{user.name}</li>)}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
