c7e4a9e6861588859764fb71cd09322944cf0bbb
jselga <jordi.selga@iescarlesvallbona.cat>
2026-01-19 12:25:03 +0100

    React: Fetch amb custom hook. useFetch

--- T3-comunicació-asíncrona/fetchReact/src/App.jsx
+++ T3-comunicació-asíncrona/fetchReact/src/App.jsx
@@ -1,19 +1,18 @@
-import { useState, useEffect } from "react";
+
 import "./App.css";
+import { useFetch } from "./useFetch";
 
 function App() {
-  const [data,setData] = useState(null);
-  const baseUrl='https://jsonplaceholder.typicode.com'
-  useEffect(()=>{
-    fetch(`${baseUrl}/users`)
-    .then((res)=> res.json())
-    .then((data)=> setData(data));
-  }, [])
+  const baseUrl = 'https://jsonplaceholder.typicode.com'
+const url=`${baseUrl}/users`;
+const {data,loading,error} = useFetch(url);
   return (
     <div className="App">
       <h1>Fetch Like a PRO</h1>
       <div className="card">
         <ul>
+          {error && <li>Error: {error}</li>}
+          {loading && <li>Loading...</li>}
           {data?.map ((user)=> <li key={user.id}>{user.name}</li>)}
         </ul>
       </div>
